e11e57df541f42d8f41d9352062e46411cfbba43
lanszweerts <stephen.lanszweert@student.howest.be>
2017-05-14 20:39:30 +0200

    Created class Sudoku

---
+++ test/sudokutestpackage/SudokuTester.java
@@ -0,0 +1,21 @@
+package sudokutestpackage;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class SudokuTester {
+    
+    @Test
+    public void canarayTest() {
+        Assert.assertTrue(true);
+    }
+
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
